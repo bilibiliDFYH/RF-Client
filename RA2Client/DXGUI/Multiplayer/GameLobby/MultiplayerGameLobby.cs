e417a8e688c8a6ed17c1ebe6ca49e88a96a6e0a7
君悦OwO <1684468347@qq.com>
2025-01-19 22:14:58 +0800

    1.5.0.49

--- RA2Client/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
+++ RA2Client/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
@@ -192,6 +192,10 @@ namespace Ra2Client.DXGUI.Multiplayer.GameLobby
                 Logger.Log("MultiplayerGameLobby: Saved games are not available!");
 
             btnRandomMap.ClientRectangle = new Rectangle(btnLockGame.Right  + 200, btnLockGame.Y, btnLockGame.Width, btnLockGame.Height);
+
+            chkExtension.Checked = true;
+            chkExtension.AllowChecking = false;
+            
         }
 
         /// <summary>

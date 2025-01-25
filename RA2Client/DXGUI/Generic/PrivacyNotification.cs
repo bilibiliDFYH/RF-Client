0f82d9311c304dd02f6202014c72e62bdc5652f0
孤梦 <mhy1630005@163.com>
2025-01-25 15:50:02 +0800

    调整首次启动时阅读许可协议所需的时间

--- RA2Client/DXGUI/Generic/PrivacyNotification.cs
+++ RA2Client/DXGUI/Generic/PrivacyNotification.cs
@@ -14,7 +14,7 @@ namespace Ra2Client.DXGUI.Generic
     public class PrivacyNotification : XNAWindow
     {
         private Timer _timer;
-        private int CountSec= 15;
+        private int CountSec= 10;
 
         private XNALabel lblDescription;
         private XNALabel lblMoreInformation;
